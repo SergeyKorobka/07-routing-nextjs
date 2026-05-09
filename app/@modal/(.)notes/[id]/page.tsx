6114527a7ef5d0f59293ce71d778313aee113b15
SergeyKorobka <s.korobka@goit.ua>
2026-05-09 22:52:56 +0300

    hw7

---
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import NotePreview from '@/components/NotePreview/NotePreview';
+import css from './Modal.module.css';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
+export default function ModalNotes() {
+  const router = useRouter();
+
+  useEffect(() => {
+    function handleClose(e: KeyboardEvent) {
+      if (e.code.toLocaleLowerCase() === 'escape') {
+        router.back();
+      }
+    }
+
+    document.addEventListener('keydown', handleClose);
+
+    return () => {
+      document.removeEventListener('keydown', handleClose);
+    };
+  }, [router]);
+
+  return (
+    <div
+      onClick={() => {
+        router.back();
+      }}
+      className={css.backdrop}
+    >
+      <div className={css.modal}>
+        <NotePreview />
+      </div>
+    </div>
+  );
+}
